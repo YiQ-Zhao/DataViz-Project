513e479069b21ef2d3b94a2c658881253d6c381d
yiqiang <yiqiang@wustl.edu>
2018-05-01 03:31:17 -0700

    updated UI and Server

--- server.R
+++ server.R
@@ -18,7 +18,17 @@ library(plotly)
 # # df$year <- as.numeric(strftime(df$`Mission Date`, format = "%Y"))
 # # df$week <- as.numeric(strftime(df$`Mission Date`, format = "%V"))
 # df$start_week <- cut(df$`Mission Date`, "week")
+
+# flight_route <- gcIntermediate(df[ , c("Takeoff Longitude", "Takeoff Latitude")],
+#                                df[ , c("Target Longitude", "Target Latitude")],
+#                                n=20,
+#                                addStartEnd=TRUE,
+#                                sp=TRUE)
+
+
+
 df <- read_csv("simplified_data.csv")
+load("flight_route.rda") # to accelerate the loading speed
 df_takeoff <- df %>% st_as_sf(., coords = c("Takeoff Longitude", "Takeoff Latitude"))
 df_target <- df %>% st_as_sf(., coords = c("Target Longitude", "Target Latitude"))
 
@@ -28,14 +38,9 @@ df_target <- df %>% st_as_sf(., coords = c("Target Longitude", "Target Latitude"
 shinyServer(function(input, output, session) {
   target_map <- SharedData$new(df_target, key = ~id, group = "grp1")
   takeoff_map <- SharedData$new(df_takeoff, key = ~id, group = "grp2")
-  # df_map <- target_map$data(TRUE)
   lf <-   leaflet(target_map) %>%
     addTiles(urlTemplate = "https://api.mapbox.com/styles/v1/yiqiang/cjglw67l500262tqmguz0efum/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoieWlxaWFuZyIsImEiOiJjamI2MjJ2aDgzZTJiMzdvMTEza25vN3czIn0.da7McqdO9XgggUE0LTCx3Q"
-    ) %>%  addPolylines(data = gcIntermediate(df[ , c("Takeoff Longitude", "Takeoff Latitude")],
-                                                  df[ , c("Target Longitude", "Target Latitude")],
-                                                  n=20,
-                                                  addStartEnd=TRUE,
-                                                  sp=TRUE),  opacity = 0.3, fillOpacity = 0.2, color = "black", stroke = TRUE, weight = 1) %>%
+    ) %>%  addPolylines(data = flight_route,  opacity = 0.3, fillOpacity = 0.2, color = "black", stroke = TRUE, weight = 1) %>%
     addCircleMarkers(fillOpacity = 0.1, color = "red", radius = 1, group = "grp1") %>%
     addCircleMarkers(data = takeoff_map, fillOpacity = 0.1, color = "blue", radius = 1, group = "grp2") %>%
     fitBounds(lng1=65.298593, lat1 = -15.124991, lng2 = -7.286442, lat2 = -173.134400) %>%
@@ -63,9 +68,14 @@ shinyServer(function(input, output, session) {
       aes(x = `Aircraft_Series`, y = `n`, fill=`Aircraft_Series`,
           text=sprintf("Aircraft Series: %s<br>Num: %s", `Aircraft_Series`, `n`)) + 
       geom_bar(stat = 'identity') + 
-      ylab("Count") + xlab("")  +
-      theme(axis.text.x = element_text(angle = 30, hjust = 1), plot.background = element_rect(fill = "grey"), 
-            plot.margin = unit(c(0.5,0.5,1,0.5), "cm"), legend.position="none") 
+      ylab("Count") + xlab("")  + ggtitle("Aircraft Series Barplot") +
+      theme(axis.text.x = element_text(angle = 30, hjust = 1, colour = "#4f4f4f"), 
+            plot.title = element_text(size = 14, face = "bold", colour = "#4f4f4f"),
+            plot.background = element_rect(fill = "#f0f0f0"), 
+            plot.margin = unit(c(0.5,0.5,1,0.5), "cm"), 
+            axis.title=element_text(size=12), 
+            legend.position="none"
+            ) 
 
     p <- ggplotly(p, tooltip="text")
     p$elementId <- NULL
@@ -73,15 +83,15 @@ shinyServer(function(input, output, session) {
   }))
   
   output$piechart <- renderPlotly(({
-    tmp_data <- tmp_selected_target %>% group_by(`TargetCountry`) %>% count()
+    tmp_data <- tmp_selected_target %>% group_by(`Target Country`) %>% count()
     tmp_data <- as.data.frame(tmp_data)
-    # p <- ggplot(tmp_data) + aes(x = `TargetCountry`, y = `n`, fill=`TargetCountry`) + geom_bar(stat = 'identity') + 
-    #   ggtitle("Distribution of Target Countries") + ylab("Count") + xlab("") + theme(legend.position="none") +
-    #   theme(axis.text.x = element_text(angle = 30, hjust = 1), plot.background = element_rect(fill = "grey"))
-    # p <- ggplotly(p)
-    # p$elementId <- NULL
-    # p
-    p2 <- plot_ly(tmp_data, labels = ~TargetCountry, values = ~n, type = 'pie')
+    textpos <- ifelse(tmp_data$n/sum(tmp_data$n, na.rm = TRUE) < 0.04, "none", "auto")
+    m <- list(l = 50, r = 50, b = 20, t = 55, pad = 2)
+    p2 <- plot_ly(tmp_data, labels = ~`Target Country`, values = ~n, type = 'pie',
+                  textposition = textpos, textinfo = 'label+percent') %>%
+          layout(paper_bgcolor = "#f0f0f0", title = "<b>Attacked Country Pie Chart<b>",
+                 margin = m, font = list(size = 14))
+    p2$elementId <- NULL
     p2
   }))
   leafletProxy("mymap") %>% clearControls()
